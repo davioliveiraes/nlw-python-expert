8dcda130d16d17cb14ba0317415095bc02ee86aa
Davi Oliveira <limad9786@gmail.com>
2024-02-07 17:02:28 -0300

    feat: Implementing View for tag creator with Http Types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,19 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+    '''
+        responsability for interacting with HTTP
+    
+    '''
+
+    def validate_and_create(self, http_request: HttpRequest):
+        # body = http_request.body
+        # product_code = body["product_code"]
+
+        # lógica de regra de negócio
+        print("Estou na minha view")
+        print(http_request)
+        # retorno http
+        return HttpResponse(status_code=2000, body={"hello": "world"})
+    
